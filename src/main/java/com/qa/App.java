449d82ca5d2ee5d62ea090273e9ae32637364bea
Nisu <nisuelza@gmail.com>
2026-03-10 16:22:49 +0000

    EDIT: Say Hello to Victoria, major version

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Everyone"));
         System.out.println(sayHelloToSomeone("Simon"));
+        System.out.println(sayHelloToSomeone("Victoria"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
